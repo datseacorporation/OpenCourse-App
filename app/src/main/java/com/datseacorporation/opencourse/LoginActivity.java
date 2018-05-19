436d5ea3973e1ac95df5d637ad9608f89f64efb7
Datsea Corporation <admin@datseacorporation.com>
2018-05-19 17:06:31 +0530

    Update 1.0.0

--- app/src/main/java/com/datseacorporation/opencourse/LoginActivity.java
+++ app/src/main/java/com/datseacorporation/opencourse/LoginActivity.java
@@ -2,12 +2,18 @@ package com.datseacorporation.opencourse;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
@@ -40,9 +46,45 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
-    
+    private void  userLogin() {
+        String email = user_email_et.getText().toString().trim();
+        String password = user_password_et.getText().toString().trim();
+        if (TextUtils.isEmpty(email)) {
+            //email is empty
+            Toast.makeText(this, "Please Enter Email", Toast.LENGTH_SHORT).show();
+            //stopping excution
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            //password is empty
+            Toast.makeText(this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+            //stopping excution
+            return;
+        }
+        // if validate ok
+        //show progress bar
+        progressDialog.setMessage("Signing In...");
+        progressDialog.show();
+        progressDialog.setCanceledOnTouchOutside(false);
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
 
+                        progressDialog.dismiss();
+                        if(task.isSuccessful()){
+                            // starting profile activity
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this, "Please Check Credentials...", Toast.LENGTH_SHORT).show();
 
+                        }
+                    }
+                });
+
+    }
 
     @Override
     public void onClick(View view) {
